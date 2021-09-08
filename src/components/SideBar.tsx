d6b3ccea5a57eaa465fb3706988f2188458fef38
HenriqueHS19 <henriquesantos.hs34@gmail.com>
2021-09-07 22:14:48 -0300

    complementary challenge completed

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,21 @@
-export function SideBar() {
+import { ReactNode } from "react";
+
+import '../styles/sidebar.scss';
+
+interface ISideBarProps {
+  children: ReactNode;
+}
+
+export function SideBar({ children } : ISideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        { children }
+      </div>
+
+    </nav>
+  );
 }
